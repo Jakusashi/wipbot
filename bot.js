308e91f3e9ea68d4e84fbe7779254b7e9748f5ed
Arcalist <seba.piwnicki@gmail.com>
2017-09-20 14:30:19 +0200

    Działa

--- bot.js
+++ bot.js
@@ -12,6 +12,10 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -25,15 +29,88 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0];
        
         args = args.splice(1);
+		// tutaj dodawać linki do remixów z Barkleyem
+		slam = ['https://www.youtube.com/watch?v=bwv_2OKxZr8',
+		'https://www.youtube.com/watch?v=uints81YYMc',
+		'https://www.youtube.com/watch?v=VjR7JZbdONs',
+		'https://www.youtube.com/watch?v=qH4NbO74A1k',
+		'https://www.youtube.com/watch?v=O4J_S3NdIUE',
+		'https://www.youtube.com/watch?v=J9FImc2LOr8',
+		'https://youtu.be/cCeovAd7iOQ',
+		'https://youtu.be/SCivYNvACCU'
+		]
+		
+		// memiczne piosenki tutaj
+		songmemes = ['https://www.youtube.com/watch?v=f3IgwLT_mYc',
+		'https://youtu.be/8CvqmD0CZao',
+		'https://youtu.be/WM-fTkO6BI4',
+		'https://youtu.be/ETfiUYij5UE',
+		'https://www.youtube.com/watch?v=K5tVbVu9Mkg',
+		'https://www.youtube.com/watch?v=9a4exztEy6g',
+		'https://www.youtube.com/watch?v=1w5_IktEDOI',
+		'https://www.youtube.com/watch?v=FGiwDwmoJLk'
+		]
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
+			case 'hilfe':
+				bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'Prefix ! - hestiastep everythingwillfreeze sandstorm rickroll karny'
+					
+                });
+				break;
+			case 'hestiastep':
+				bot.sendMessage({
+                    to: channelID,
+                    message: 'https://www.youtube.com/watch?v=nZsC5p9M3p8'
+					
+                });
+				break;
+			case 'everythingwillfreeze':
+				bot.sendMessage({
+                    to: channelID,
+                    message: 'https://www.youtube.com/watch?v=lkicMsn-s_8'
+					
                 });
             break;
-            // Just add any case commands if you want to..
+			
+			case 'rickroll':
+				bot.sendMessage({
+					to: channelID,
+                    message: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+									
+				})
+				break;
+			case 'sandstorm':
+				bot.sendMessage({
+					to: channelID,
+                    message: 'https://www.youtube.com/watch?v=y6120QOlsfU'
+									
+				})
+			break;
+			
+			case 'karny':
+				bot.sendMessage({
+					to: channelID,
+                    message: 'https://cdn.discordapp.com/attachments/359694153791176706/360027584958038019/received_967748513299093.jpeg'							
+				})
+				
+			break;
+			
+			case 'slam':
+				var i = randomIntInc(1,8)
+				bot.sendMessage({
+					to: channelID,
+					message: slam[i]
+				})
+				break;
+				
+			case 'memesong':
+			var i = randomIntInc(1,8)
+				bot.sendMessage({
+					to: channelID,
+					message: songmemes[i]
+				})
+				break;
          }
      }
 });
